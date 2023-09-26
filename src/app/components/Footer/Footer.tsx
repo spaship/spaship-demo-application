6861c35f69b36c9d2db0a43b066b6a8b04c3a6e0
pjain24 <priyadarshnajain@gmail.com>
2023-09-26 09:42:45 +0530

    navbar and footer changes

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -6,8 +6,8 @@ function Footer() {
       <footer className="custom-footer">
         <div className="container p-4">
           <p>
-            &copy; {new Date().getFullYear()} SPAship, All rights reserved{" "}
-            {process.env.NEXT_PUBLIC_VERSION}
+            &copy; {new Date().getFullYear()} {process.env.NEXT_PUBLIC_VERSION},
+            All rights reserved{" "}
           </p>
         </div>
       </footer>
